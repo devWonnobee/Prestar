6c4d469c981d14718c8912622f2bfa4b2babe5bf
Michael S. <dev.micsaltat@gmail.com>
2024-05-31 23:51:28 +0200

    Initial commit

---
+++ PrestarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrestarApp.swift
+//  Prestar
+//
+//  Created by Michael Saltat on 31/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PrestarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
